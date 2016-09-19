dc49081e541a6aeb2a885b3605c1ee980cb4b035
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2016-09-19 10:50:37 +0200

    added routes for profile pages and their controller + profiles' view folder with views inside

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get "/users/:id", to: "profiles#show", as: :profile
+  get "/profile/edit", to: "profiles#edit", as: :edit_profile
+  patch "/profile", to: "profiles#update", as: :update_profile
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
