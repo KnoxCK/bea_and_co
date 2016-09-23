050d67c687784e58c49cb70312420e7ff3394d2f
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2016-09-23 11:56:55 +0200

    added chaperone page and route and method on pagescontroller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,7 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def chaperone
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "/users/:id", to: "profiles#show", as: :profile
   get "/profile/edit", to: "profiles#edit", as: :edit_profile
   patch "/profile", to: "profiles#update", as: :update_profile
-
+  get '/chaperone', to: 'pages#chaperone', as: :chaperone
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
