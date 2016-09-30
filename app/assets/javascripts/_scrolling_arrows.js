6f35a0f02b0a1d4eed5adb53fdef8e582777b27e
Clarissa Knox <knox.ck@gmail.com>
2016-09-30 14:22:24 +0100

    done logo arrows left and right

--- app/assets/javascripts/_scrolling_arrows.js
+++ app/assets/javascripts/_scrolling_arrows.js
@@ -1,5 +1,6 @@
+// Testimonial section
 $(document).on('click', '.test-right-arrow', function(e) {
-  	$(".test-line-one").addClass("hidden");
+  $(".test-line-one").addClass("hidden");
 	$(".test-line-two").removeClass("hidden");
 	$(".test-right-arrow").addClass("hidden");
 	$(".test-left-arrow").removeClass("hidden");
@@ -13,4 +14,24 @@ $(document).on('click', '.test-left-arrow', function(e) {
 	e.preventDefault();
 });
 
-  	
+// Logos section
+$(document).on('click', '.logos-right-arrow', function(e) {
+ 	$(".logos-line-one").addClass("hidden");
+ 	$(".logos-line-two").removeClass("hidden");
+	e.preventDefault();
+});
+$(document).on('click', '.logos-left-arrow-two', function(e) {
+ 	$(".logos-line-one").removeClass("hidden");
+ 	$(".logos-line-two").addClass("hidden");
+	e.preventDefault();
+});
+$(document).on('click', '.logos-right-arrow-two', function(e) {
+ 	$(".logos-line-two").addClass("hidden");
+ 	$(".logos-line-three").removeClass("hidden");
+	e.preventDefault();
+});
+$(document).on('click', '.logos-left-arrow-three', function(e) {
+ 	$(".logos-line-two").removeClass("hidden");
+ 	$(".logos-line-three").addClass("hidden");
+	e.preventDefault();
+});
