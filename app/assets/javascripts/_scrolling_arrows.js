845bea45d403bcb5972d9cf78bbdff7fb11efb5a
Clarissa Knox <knox.ck@gmail.com>
2016-11-23 17:32:02 +0000

    ready to send

--- app/assets/javascripts/_scrolling_arrows.js
+++ app/assets/javascripts/_scrolling_arrows.js
@@ -28,48 +28,70 @@ $(document).on('click', '.test-arrow-l-two', function(e) {
 	e.preventDefault();
 });
 
-// Logos section 1
-$(document).on('click', '.logos-left-arrow', function(e) {
+// Partners section 1
+$(document).on('click', '.part-l-arrow-js', function(e) {
  	$(".logos-line-one").addClass("hidden");
  	$(".logos-line-four").removeClass("hidden");
 	e.preventDefault();
 });
-$(document).on('click', '.logos-right-arrow', function(e) {
+$(document).on('click', '.part-r-arrow-js', function(e) {
  	$(".logos-line-one").addClass("hidden");
  	$(".logos-line-two").removeClass("hidden");
 	e.preventDefault();
 });
-// Logos section 2
-$(document).on('click', '.logos-left-arrow-two', function(e) {
+// Partners section 2
+$(document).on('click', '.part-l-arrow-js-two', function(e) {
  	$(".logos-line-one").removeClass("hidden");
  	$(".logos-line-two").addClass("hidden");
 	e.preventDefault();
 });
-$(document).on('click', '.logos-right-arrow-two', function(e) {
+$(document).on('click', '.part-r-arrow-js-two', function(e) {
  	$(".logos-line-two").addClass("hidden");
  	$(".logos-line-three").removeClass("hidden");
 	e.preventDefault();
 });
-// Logos section 3
-$(document).on('click', '.logos-right-arrow-three', function(e) {
+// Partners section 3
+$(document).on('click', '.part-r-arrow-js-three', function(e) {
  	$(".logos-line-three").addClass("hidden");
  	$(".logos-line-four").removeClass("hidden");
 	e.preventDefault();
 });
-$(document).on('click', '.logos-left-arrow-three', function(e) {
+$(document).on('click', '.part-l-arrow-js-three', function(e) {
  	$(".logos-line-two").removeClass("hidden");
  	$(".logos-line-three").addClass("hidden");
 	e.preventDefault();
 });
-// Logos section 4
-$(document).on('click', '.logos-right-arrow-four', function(e) {
+// Partners section 4
+$(document).on('click', '.part-r-arrow-js-four', function(e) {
  	$(".logos-line-four").addClass("hidden");
  	$(".logos-line-one").removeClass("hidden");
 	e.preventDefault();
 });
-$(document).on('click', '.logos-left-arrow-four', function(e) {
+$(document).on('click', '.part-l-arrow-js-four', function(e) {
  	$(".logos-line-three").removeClass("hidden");
  	$(".logos-line-four").addClass("hidden");
 	e.preventDefault();
 });
 
+// Press section 1
+$(document).on('click', '.press-l-arrow-js', function(e) {
+ 	$(".press-logos-line-one").addClass("hidden");
+ 	$(".press-logos-line-two").removeClass("hidden");
+	e.preventDefault();
+});
+$(document).on('click', '.press-r-arrow-js', function(e) {
+ 	$(".press-logos-line-one").addClass("hidden");
+ 	$(".press-logos-line-two").removeClass("hidden");
+	e.preventDefault();
+});
+// Press section 2
+$(document).on('click', '.press-l-arrow-js-two', function(e) {
+ 	$(".press-logos-line-one").removeClass("hidden");
+ 	$(".press-logos-line-two").addClass("hidden");
+	e.preventDefault();
+});
+$(document).on('click', '.press-r-arrow-js-two', function(e) {
+ 	$(".press-logos-line-two").addClass("hidden");
+ 	$(".press-logos-line-one").removeClass("hidden");
+	e.preventDefault();
+});
