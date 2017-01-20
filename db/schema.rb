0177ef033ea0583e5cc85979145c992246e61d28
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2017-01-20 09:27:12 +0100

    generates partner model, added photo thing and registered with AA

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214111605) do
+ActiveRecord::Schema.define(version: 20170120082055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(version: 20161214111605) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "rating"
+  end
+
+  create_table "partners", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "photo"
   end
 
   create_table "service_paragraphs", force: :cascade do |t|
