b08b7df23aba5b8eed731f164c09efa45be54378
Clarissa Knox <knox.ck@gmail.com>
2017-01-23 12:32:17 +0000

    next

--- app/assets/javascripts/_scroll_navbar.js
+++ app/assets/javascripts/_scroll_navbar.js
@@ -4,10 +4,8 @@ $(window).scroll(function() {
     if (scroll > $(window).height() * 0.99) {
         $("navbar-beaco").addClass("hidden");
         $(".navbar-beaco-scroll").removeClass("hidden");
-        $(".navbar-beaco-mobile").toggleClass('opened');
     } else if (scroll < $(window).height()) {
         $("navbar-beaco").removeClass("hidden");
         $(".navbar-beaco-scroll").addClass("hidden");
-                $(".navbar-beaco-mobile").toggleClass('opened');
     }
 });
