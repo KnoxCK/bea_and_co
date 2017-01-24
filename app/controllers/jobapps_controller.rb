be3b35a2a9a5c76cd73b1bd17272374e2b4c256d
Clarissa Knox <knox.ck@gmail.com>
2017-01-24 12:47:32 +0000

    preparing for lectures

--- app/controllers/jobapps_controller.rb
+++ app/controllers/jobapps_controller.rb
@@ -1,8 +1,11 @@
 class JobappsController < ApplicationController
 	def new
-		
+		@jobapp = Jobapp.new 
 	end
 
 	def create
+		@jobapp = Jobapp.new(params[:jobapps])
+		@jobapp.save
+		redirect_to root_path
 	end
 end
