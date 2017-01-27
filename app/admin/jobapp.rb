e0b8849c16497dbb2161a0ce71c91bf5c48d2475
Clarissa Knox <knox.ck@gmail.com>
2017-01-27 12:03:26 +0000

    gemfile

--- app/admin/jobapp.rb
+++ app/admin/jobapp.rb
@@ -1,61 +1,6 @@
 ActiveAdmin.register Jobapp do
 
-	index do
-    selectable_column
-    column :first_name
-    column :last_name
-    column :email
-    column :phone_num
-    column :where_based
-    column :past_experience
-    column :fav_kids_book
-    column :super_power
-    column :known_employee
-    column :dbs_registered
-    column :self_employed
-    column :accepts_data_treatment
-    actions
-  end
 
-   form do |f|
-    f.inputs "Name" do
-      f.input :first_name
-    end
-    f.inputs "Since" do
-      f.input :last_name
-    end
-    f.inputs "Comment" do
-      f.input :email
-    end
-    f.inputs "Relation" do
-      f.input :phone_num
-    end
-    f.inputs "Location" do
-      f.input :where_based
-    end
-    f.inputs "Comment" do
-      f.input :past_experience
-    end
-    f.inputs "Relation" do
-      f.input :fav_kids_book
-    end
-    f.inputs "Location" do
-      f.input :super_power
-    end
-    f.inputs "Comment" do
-      f.input :known_employee
-    end
-    f.inputs "Relation" do
-      f.input :dbs_registered
-    end
-    f.inputs "Location" do
-      f.input :self_employed
-    end
-    f.inputs "Location" do
-    f.input :accepts_data_treatment
-    end
-    f.actions
-  end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -68,7 +13,7 @@ ActiveAdmin.register Jobapp do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-permit_params :first_name, :last_name, :email, :phone_num, :where_based, :past_experience, :fav_kids_book, :super_power, :known_employee, :dbs_registered, :self_employed
+permit_params :first_name, :last_name, :email, :phone_num, :where_based, :past_experience, :fav_kids_book, :super_power, :known_employee, :dbs_registered, :self_employed, :upload_cv
 
 end
 
