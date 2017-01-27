b576577c300e80c409c16d21eadad035ca75b324
Clarissa Knox <knox.ck@gmail.com>
2017-01-27 10:57:36 +0000

    params

--- app/controllers/jobapps_controller.rb
+++ app/controllers/jobapps_controller.rb
@@ -1,5 +1,5 @@
 class JobappsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [ :new, :create ]
 
 	def new
 		@jobapp = Jobapp.new
