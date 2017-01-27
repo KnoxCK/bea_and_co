e8c1a2b0e5bafa041e489cc491c64adfe49032e9
Clarissa Knox <knox.ck@gmail.com>
2017-01-27 11:16:02 +0000

    active admin

--- app/admin/jobapp.rb
+++ app/admin/jobapp.rb
@@ -1,5 +1,57 @@
 ActiveAdmin.register Jobapp do
 
+	index do
+    selectable_column
+    column :first_name
+    column :last_name
+    column :email
+    column :phone_num
+    column :where_based
+    column :past_experience
+    column :fav_kids_book
+    column :super_power
+    column :known_employee
+    column :dbs_registered
+    column :self_employed
+    actions
+  end
+
+   form do |f|
+    f.inputs "Name" do
+      f.input :first_name
+    end
+    f.inputs "Since" do
+      f.input :last_name
+    end
+    f.inputs "Comment" do
+      f.input :email
+    end
+    f.inputs "Relation" do
+      f.input :phone_num
+    end
+    f.inputs "Location" do
+      f.input :where_based
+    end
+    f.inputs "Comment" do
+      f.input :past_experience
+    end
+    f.inputs "Relation" do
+      f.input :fav_kids_book
+    end
+    f.inputs "Location" do
+      f.input :super_power
+    end
+    f.inputs "Comment" do
+      f.input :known_employee
+    end
+    f.inputs "Relation" do
+      f.input :dbs_registered
+    end
+    f.inputs "Location" do
+      f.input :self_employed
+    end
+    f.actions
+  end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
