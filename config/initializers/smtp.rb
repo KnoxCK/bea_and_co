57ab756b722e42e3aaa1f6c833d5154cc74f1e73
Arthur Littmann <arthur.littmann@gmail.com>
2017-01-30 17:35:52 +0000

    set heroku domain in production.rb and smtp

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   address: ENV['smtp.postmarkapp.com'],
   port: '25',
-  domain: 'beaandco.com',
+  domain: 'bea-and-co.herokuapp.com',
   user_name: ENV['5df4baf9-bd03-4c88-9295-d73dbdfb9e1e'],
   password: ENV['5df4baf9-bd03-4c88-9295-d73dbdfb9e1e'],
   authentication: :cram_md5,
