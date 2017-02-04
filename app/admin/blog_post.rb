a67edc7b3a7aa55ef263684b8d69c05ee06a8bb4
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2017-02-04 08:49:28 +0100

    added migration and put photo in the permit_params of blog_post

--- app/admin/blog_post.rb
+++ app/admin/blog_post.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register BlogPost do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :title, :content
+permit_params :title, :content, :photo
 #
 # or
 #
