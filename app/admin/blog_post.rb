64312712593c17baf7183ec18961ff609f88f88c
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-07-24 13:10:04 +0100

    shit

--- app/admin/blog_post.rb
+++ app/admin/blog_post.rb
@@ -4,6 +4,7 @@ ActiveAdmin.register BlogPost do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 permit_params :title, :content, :photo
+
 #
 # or
 #
