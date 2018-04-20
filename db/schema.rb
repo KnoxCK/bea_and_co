d39ce64f9bcb856e595c30b4fa5d3d3a4858dc05
Clarissa Knox <knox.ck@gmail.com>
2018-04-20 09:45:03 +0100

    blog

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,20 @@ ActiveRecord::Schema.define(version: 20170204073941) do
     t.string   "photo"
   end
 
+  create_table "bootsy_image_galleries", force: :cascade do |t|
+    t.string   "bootsy_resource_type"
+    t.integer  "bootsy_resource_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "bootsy_images", force: :cascade do |t|
+    t.string   "image_file"
+    t.integer  "image_gallery_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "first_paragraphs", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
@@ -96,6 +110,12 @@ ActiveRecord::Schema.define(version: 20170204073941) do
     t.string   "icon"
   end
 
+  create_table "team_rows", force: :cascade do |t|
+    t.integer  "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
@@ -125,8 +145,11 @@ ActiveRecord::Schema.define(version: 20170204073941) do
     t.text     "exp_with_kids"
     t.boolean  "admin",                  default: false, null: false
     t.string   "role"
+    t.integer  "team_row_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["team_row_id"], name: "index_users_on_team_row_id", using: :btree
   end
 
+  add_foreign_key "users", "team_rows"
 end
